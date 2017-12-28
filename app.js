c67aefcea5c137c3230f1be694d36fc57277a585
Anthony Campos <anthonydevelops@gmail.com>
2017-12-27 18:08:34 -0800

    Functioning, but many bugs

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const city = document.querySelector('#city');
-const temperature = document.querySelector('#temp');
-const url = 'https://fcc-weather-api.glitch.me/';
-
-const showCity = function (position) {
-  city.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
-}
+const city = document.querySelector("#city");
+const temperature = document.querySelector("#weather")
+const tempImage = document.querySelector("#icon");
 
 function getLocation() {
   if("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(showCity);
+    navigator.geolocation.getCurrentPosition(getData);
   } else {
     city.innerHTML = "Geolocation not supported";
   }
+}
+
+const getData = function (position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const url = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+  accessApi(url);
+}
+
+function accessApi(url) {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      let condition = data.weather[0].main;
+      let temp = data.main.temp;
+      let weatherIcon = data.weather[0].icon;
+      showData(condition, temp, weatherIcon);
+    })
+    .catch(function(e) {
+      console.log(e);
+    });
+}
+
+function showData(cond, temp, image) {
+  temperature.innerHTML = cond + " " + temp;
+  tempImage.src = image;
 }
