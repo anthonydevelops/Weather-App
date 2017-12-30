2538e69b1cfb57994f336d1be0fb8a5cb8de63a3
Anthony Campos <anthonydevelops@gmail.com>
2017-12-29 17:17:04 -0800

    Added animated Skycons and fixed C/F toggle

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const city = document.querySelector("#city");
-const temperature = document.querySelector("#weather")
-const main = document.querySelector(".data");
+const weather = document.querySelector("#weather")
+let tempType = 0;
+let temperature = 0;
+let condition = "";
+let description = "";
+
+//Using Skycons to display animated weather icons
+const skycons = new Skycons({"color": "#000"});
+skycons.play();
 
 //On start, check if geolocation is available. If not, report it.
 window.onload = () => {
@@ -24,10 +30,10 @@ function accessApi(url) {
   fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
-      let condition = data.weather[0].main;
-      let temp = data.main.temp;
-      let weatherIcon = data.weather[0].icon;
-      showData(condition, temp, weatherIcon);
+      condition = data.weather[0].main;
+      temperature = data.main.temp;
+      description = data.weather[0].description;
+      showData(condition, temperature, description);
     })
     .catch(function(e) {
       console.log(e);
@@ -35,9 +41,48 @@ function accessApi(url) {
 }
 
 //Display weather data & image
-function showData(cond, temp, image) {
-  const newImage = document.createElement("IMG");
-  newImage.setAttribute("src", image);
-  main.appendChild(newImage);
-  temperature.innerHTML = cond + " " + temp;
+function showData(condition, temperature, description) {
+  weather.innerHTML = description + " " + Math.floor(temperature) + "&#x2103";
+  //Update animated Skycon image to match condition
+  switch(condition.toLowerCase()) {
+    case "rain":
+      skycons.set("animated-icon", Skycons.RAIN);
+      break;
+    case "sunny":
+      skycons.set("animated-icon", Skycons.CLEAR_DAY);
+      break;
+    case "haze":
+      skycons.set("animated-icon", Skycons.FOG);
+      break;
+    case "cloudy":
+      skycons.set("animated-icon", Skycons.CLOUDY);
+      break;
+    case "snow":
+      skycons.set("animated-icon", Skycons.SNOW);
+      break;
+    case "wind":
+      skycons.set("animated-icon", Skycons.WIND);
+      break;
+    case "clear":
+      skycons.set("animated-icon", Skycons.CLEAR_DAY);
+      break;
+    default:
+      skycons.set("animated-icon", Skycons.CLEAR_DAY);
+  }
+}
+
+//Button function that changes C to F, F to C.
+function changeTemp() {
+  if(tempType === 0) {
+    //C to F equation
+    temperature = temperature * (1.8) + 32;
+    weather.innerHTML = description + " " + Math.floor(temperature) + "&#x2109";
+    tempType = 1;
+  }
+  else if(tempType === 1) {
+    //F to C equation
+    temperature = (temperature - 32) * .5556;
+    weather.innerHTML = description + " " + Math.floor(temperature) + "&#x2103";
+    tempType = 0;
+  }
 }
