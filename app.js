9db7deee12763445a1c577907fdea73cf4f7cd9b
Anthony Campos <anthonydevelops@gmail.com>
2017-12-30 21:46:12 -0800

    Finished weather app, will come back to finish optionals later

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const city = document.querySelector("#city");
 const weather = document.querySelector("#weather")
 let tempType = 0;
 let temperature = 0;
 let condition = "";
 let description = "";
+let geocoder = {};
 
 //Using Skycons to display animated weather icons
 const skycons = new Skycons({"color": "#000"});
@@ -19,21 +21,23 @@ window.onload = () => {
 
 //Capture lat & long, then access the api which will display local weather data
 const getData = (position) => {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  const url = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude + "&lon=" + longitude;
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lng;
   accessApi(url);
+  displayCity(lat, lng);
 }
 
 //Fetch data from FreeCodeCamp api
 function accessApi(url) {
   fetch(url)
+  //Revert fetch to json object
     .then((resp) => resp.json())
     .then(function(data) {
       condition = data.weather[0].main;
       temperature = data.main.temp;
       description = data.weather[0].description;
-      showData(condition, temperature, description);
+      showWeather(condition, temperature, description);
     })
     .catch(function(e) {
       console.log(e);
@@ -41,7 +45,7 @@ function accessApi(url) {
 }
 
 //Display weather data & image
-function showData(condition, temperature, description) {
+function showWeather(condition, temperature, description) {
   weather.innerHTML = description + " " + Math.floor(temperature) + "&#x2103";
   //Update animated Skycon image to match condition
   switch(condition.toLowerCase()) {
@@ -69,6 +73,31 @@ function showData(condition, temperature, description) {
     default:
       skycons.set("animated-icon", Skycons.CLEAR_DAY);
   }
+}
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+}
+
+//Google maps reverse geolocation
+//Stack Overflow reference:
+//        https://stackoverflow.com/questions/6797569/get-city-name-using-geolocation
+function displayCity(lat, lng) {
+  //Create a new maps object to track lat&lng
+  const latlng = new google.maps.LatLng(lat, lng);
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if(results[1]) {
+        //Retrieving 3rd result of the formatted address with city, state, country
+        const address = results[2].formatted_address;
+        city.innerHTML = address;
+      } else {
+        console.log("No results found");
+      }
+    } else {
+      console.log("Geocoder failed");
+    }
+  });
 }
 
 //Button function that changes C to F, F to C.
